d1ae8a65ef50fa10976aee900c32e526c65e3c40
TheMizor <simongirard702@gmail.com>
2025-10-29 23:45:28 +0100

    mise à jour du point d'entrée avec interface graphique
    - Lance ChessUI directement
    - Configuration pour environnement graphique

--- src/main/java/com/chess/ChessGame.java
+++ src/main/java/com/chess/ChessGame.java
@@ -1,7 +1,16 @@
 package com.chess;
 
+import com.chess.ui.ChessUI;
+
+/**
+ * Point d'entrée principal du jeu d'échecs.
+ */
 public class ChessGame {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Lancer l'interface graphique
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            ChessUI chessUI = new ChessUI();
+            chessUI.setVisible(true);
+        });
     }
 }
